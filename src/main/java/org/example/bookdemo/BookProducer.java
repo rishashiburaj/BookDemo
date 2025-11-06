c0628b13cf0385a0b45c0917d7c349da07e8f9cd
rishashiburaj <risha.shiburaj@gmail.com>
2025-11-06 14:22:43 -0500

    exception updated again

--- src/main/java/org/example/bookdemo/BookProducer.java
+++ src/main/java/org/example/bookdemo/BookProducer.java
@@ -1,15 +1,11 @@
 package org.example.bookdemo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.common.KafkaException;
-import org.apache.kafka.common.errors.InterruptException;
 import org.example.bookdemo.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.ExecutionException;
 
 @Service
 @Slf4j
